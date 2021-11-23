00c3768481636a3764f6a1a0aa6f3cf464e105af
Or Samu <or_sti@windowslive.com>
2021-11-23 22:10:21 +0200

    second algo

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -31,10 +31,78 @@ string Algorithms::ListAlgo(Graph inputGraph)
 
 string Algorithms::MatriceAlgo(Graph inputGraph)
 {
-	return "NO";
+	int** matriceGraph = inputGraph.GetMatriceGraph();
+	int** gPowIn2, **gPowIn3;
+	int A = NULL;
+	int B = NULL;
+	int C = NULL;
+	int size = inputGraph.GetSize() + 1;
+	string triangle="";
+	gPowIn2 = multiply(matriceGraph, matriceGraph,size);
+	gPowIn3 = multiply(matriceGraph, gPowIn2,size);
+	for (int i = 1; i <= size; i++)
+	{
+		if (gPowIn3[i][i] > 0)
+		{
+			A = gPowIn3[i][i];
+			break;
+		}
+	}
+	if (A != NULL)
+	{
+		for (int i = 1; i <= size; i++)
+		{
+			if (i != A && gPowIn2[i][A] != 0)
+			{
+				if (matriceGraph[A][i] != 0)
+				{
+					B = i;
+					for (int j = 0; j <= size; j++)
+					{
+						if (matriceGraph[B][j] != 0 && matriceGraph[j][A] != 0)
+						{
+							C = j;
+						}
+					}
+				}
+			}
+		}
+		triangle = to_string(A) + " " + to_string(B) + " " + to_string(C) + " " + to_string(A);
+	}
+	else triangle = "NO";
+
+	releaseMatrice(gPowIn2,size);
+	releaseMatrice(gPowIn3,size);
+
+	return triangle;
 }
 
 string Algorithms::AYZAlgo(Graph inputGraph)
 {
 	return "NO";
+}
+
+int** multiply(int** a, int** b, int size)
+{
+	int** mul = new int* [size];
+	for (int i = 0; i <= size; i++)
+	{
+		mul[i] = new int[size];
+		for (int j = 0; j < 3; j++)
+		{
+			mul[i][j] = 0;
+			for (int k = 0; k < 3; k++)
+				mul[i][j] += a[i][k] * b[k][j];
+		}
+	}
+	return mul;
+}
+
+void releaseMatrice(int **matriceToRelease,int size)
+{
+	for (int i = 0; i <= size; i++)
+	{
+		delete[] matriceToRelease[i];
+	}
+	delete[] matriceToRelease;
 }
